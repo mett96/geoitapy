2b9623abd83d06baf45c714beec01387a92b9048
Mattia <mattia.sanchioni.dev@gmail.com>
2021-01-30 02:38:04 +0100

    package - download the database automatically

---
+++ geoitapy/__init__.py
@@ -0,0 +1,4 @@
+from geoitapy.util import __version__, __version_info__, get_version
+from geoitapy.db_manager import download_database
+
+download_database()
---
+++ geoitapy/util.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.0"
+__version_info__ = (0, 1, 0)
+
+import loggerpy
+
+logger_level = loggerpy.Level.DEBUG
+
+
+def get_version():
+    return __version__
